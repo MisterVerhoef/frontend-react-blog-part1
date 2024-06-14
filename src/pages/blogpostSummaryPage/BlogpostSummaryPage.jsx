3dc332ac3e9b060508245b3249622febfd80707e
MisterVerhoef <frankverhoef@outlook.com>
2024-06-14 15:39:36 +0200

    all blog post are now shown on the summary page

--- src/pages/blogpostSummaryPage/BlogpostSummaryPage.jsx
+++ src/pages/blogpostSummaryPage/BlogpostSummaryPage.jsx
@@ -2,10 +2,27 @@ import './BlogpostSummaryPage.css';
 import posts from "../../constants/data.json";
 
 function BlogpostSummaryPage() {
-   console.log(posts)
+
+
+    console.log(posts)
+    console.log(posts.title)
     return (
         <div>
+            <main>
             <h1>BlogpostSummaryPage</h1>
+
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <h2>{post.title}</h2>
+                        <h3>{post.author}</h3>
+                        <h4>{post.subtitle}</h4>
+                        <p>{post.content}</p>
+                    </li>
+
+                ))}
+            </ul>
+            </main>
         </div>
     );
 }
