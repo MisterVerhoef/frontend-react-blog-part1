2b5aa4c2ae290baaeeff34f23286f68cbb104a97
MisterVerhoef <frankverhoef@outlook.com>
2024-06-15 16:47:25 +0200

    correct layout for blog summary and links for blogs now work

--- src/pages/blogpost/BlogPost.jsx
+++ src/pages/blogpost/BlogPost.jsx
@@ -1,15 +1,21 @@
 import posts from "../../constants/data.json";
 import './BlogPost.css'
+import {useParams} from "react-router-dom";
 
 function BlogPost() {
-    console.log(posts)
-    return(
+    console.log(posts.id)
 
+    const {id} = useParams();
+    const post = posts.find((post) => post.id === parseInt(id));
+
+    return (
         <div className="blog-post">
-            <h1>Blog post</h1>
+                        <h1>{post.title}</h1>
+                        <h3>{post.author}</h3>
+                        <h4>{post.subtitle}</h4>
+                        <p>{post.content}</p>
         </div>
-    )
-
+    );
 }
 
 export default BlogPost;
--- src/pages/blogpostSummaryPage/BlogpostSummaryPage.jsx
+++ src/pages/blogpostSummaryPage/BlogpostSummaryPage.jsx
@@ -1,24 +1,23 @@
 import './BlogpostSummaryPage.css';
 import posts from "../../constants/data.json";
+import {Link} from "react-router-dom";
 
 function BlogpostSummaryPage() {
+    console.log(posts);
 
 
-    console.log(posts)
-    console.log(posts.title)
     return (
         <div>
-            <h1>Bekijk alle 17 post op het platform</h1>
+            <h1>Bekijk alle {posts.length} post op het platform</h1>
 
             <ul>
                 {posts.map((post) => (
                     <li key={post.id}>
-                        <h2>{post.title}</h2>
-                        <h3>{post.author}</h3>
-                        <h4>{post.subtitle}</h4>
-                        <p>{post.content}</p>
+                        <h2>
+                            <Link to={`/posts/${post.id}`}>{post.title}</Link> ({post.author})
+                        </h2>
+                        <p>{post.comments} reacties - {post.shares} gedeeld</p>
                     </li>
-
                 ))}
             </ul>
         </div>
