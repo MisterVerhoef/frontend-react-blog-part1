cd0c664c7d169658f20dd8055c79c46d6b5255ba
MisterVerhoef <frankverhoef@outlook.com>
2024-06-17 14:04:27 +0200

    added proper data layout, now I need to push it to data.json

--- src/components/createBlogPost/CreateBlogPost.jsx
+++ src/components/createBlogPost/CreateBlogPost.jsx
@@ -6,32 +6,47 @@ function CreateBlogPost() {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const [data, setData] = useState("");
 
+    const onSubmit= (formData) => {
+        const fomattedData = {
+            title: formData.title,
+            subtitle: formData.subtitle,
+            author: formData.content,
+            content: formData.author,
+            created_at: new Date().toISOString(),
+            readTime: Math.ceil(formData.content.split(" ").length / 100),
+            comments: 0,
+            shares: 0,
+        };
+        setData(JSON.stringify(fomattedData));
+    };
+
     return (
         <>
             <h1>Post toevoegen</h1>
 
-            <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 Titel
-                <input {...register("title", { required: true })} />
+                <input {...register("title", {required: true})} />
                 {errors.title && <span>Dit veld is verplicht</span>}
 
                 Subtitle
-                <input {...register("subtitle", { required: true })} />
+                <input {...register("subtitle", {required: true})} />
                 {errors.subtitle && <span>Dit veld is verplicht</span>}
 
                 Naam en Achternaam
-                <input {...register("author", { required: true })} />
+                <input {...register("author", {required: true})} />
                 {errors.author && <span>Dit veld is verplicht</span>}
 
                 Blogpost
-                <textarea {...register("content", { required: true })} />
+                <textarea {...register("content", {required: true})} />
                 {errors.content && <span>Dit veld is verplicht</span>}
 
-                <p>{data}</p>
                 <button type="submit">
                     toevoegen
                 </button>
             </form>
+
+            <p>{data}</p>
         </>
     );
 }
