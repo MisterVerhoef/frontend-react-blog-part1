a062a380812389ba066ab54eab82fc201f7a9e57
MisterVerhoef <frankverhoef@outlook.com>
2024-06-17 13:51:59 +0200

    added required text fields

--- src/components/createBlogPost/CreateBlogPost.jsx
+++ src/components/createBlogPost/CreateBlogPost.jsx
@@ -1,26 +1,32 @@
 import "./CreateBlogPost.css"
 import { useForm } from "react-hook-form";
-import {useState} from "react";
+import { useState } from "react";
 
 function CreateBlogPost() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const [data, setData] = useState("");
 
     return (
         <>
-        <h1>Post toevoegen</h1>
+            <h1>Post toevoegen</h1>
 
             <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
                 Titel
-                <input {...register("title")}  />
+                <input {...register("title", { required: true })} />
+                {errors.title && <span>Dit veld is verplicht</span>}
+
                 Subtitle
-                <input {...register("subtitle")} />
+                <input {...register("subtitle", { required: true })} />
+                {errors.subtitle && <span>Dit veld is verplicht</span>}
+
                 Naam en Achternaam
-                <input
-                    {...register("author")}
-                />
+                <input {...register("author", { required: true })} />
+                {errors.author && <span>Dit veld is verplicht</span>}
+
                 Blogpost
-                <textarea {...register("content")} />
+                <textarea {...register("content", { required: true })} />
+                {errors.content && <span>Dit veld is verplicht</span>}
+
                 <p>{data}</p>
                 <button type="submit">
                     toevoegen
